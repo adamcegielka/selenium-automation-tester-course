10bc5f8b8def254fdeed641b4f48109c0c8ed7a8
Adam Cegiełka <adamcegielka@tlen.pl>
2024-08-24 19:57:16 +0200

    test: add test to open Booking Page

---
+++ src/test/java/OpenBookingPage.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenBookingPage {
+
+    @Test
+    public void openBookingPage() {
+        WebDriver driver = getDriver("chrome");
+        driver.get("https://www.booking.com/");
+
+        // Zamykanie przeglądarki po teście
+        driver.quit();
+    }
+
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                return new ChromeDriver();
+            case "firefox":
+                return new FirefoxDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+}
