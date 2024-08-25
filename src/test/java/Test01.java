6143f1861e7570b9930437b72701d08c2dfa19f3
Adam Cegiełka <adamcegielka@tlen.pl>
2024-08-25 15:10:28 +0200

    test: add test with google.com and assertion

---
+++ src/test/java/Test01.java
@@ -0,0 +1,60 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Test01 {
+
+    @Test
+    public void openGooglePage() {
+        WebDriver driver = getDriver("firefox");
+        driver.get("https://www.google.com/");
+
+        // Akceptacja polityki cookies"
+        WebElement agreeButton = driver.findElement(By.xpath("//div[text()='Zaakceptuj wszystko']"));
+        agreeButton.click();
+
+        // Pole wyszukiwania
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Selenium");
+        searchField.sendKeys(Keys.ENTER);
+
+        // Oczekiwanie na załadowanie strony i sprawdzenie tytułu
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Selenium"));
+
+        // Asercja na podstawie tytułu strony
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("Selenium"), "Tytuł strony nie zawiera słowa 'Selenium'.");
+
+        driver.quit();
+    }
+
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--incognito");
+                return new ChromeDriver(options);
+            case "firefox":
+                return new FirefoxDriver();
+            case "safari":
+                return new SafariDriver();
+            case "edge":
+                return new EdgeDriver();
+            case "ie":
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+}
