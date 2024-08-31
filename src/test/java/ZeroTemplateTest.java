dcc784a8a008f49bf3c09536ca125c248e2b5c37
Adam Cegiełka <adamcegielka@tlen.pl>
2024-08-31 17:12:46 +0200

    test: add template for tests

--- src/test/java/ZeroTemplateTest.java
+++ src/test/java/ZeroTemplateTest.java
@@ -1,20 +1,26 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class ZeroTemplate {
+public class ZeroTemplateTest {
 
     WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        driver = BrowserDriverManager.getDriver("firefox");
         driver.get("http://seleniumdemo.com/");
     }
 
     @AfterMethod
-    public void tearDown() {}
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstNameTest() {}
+
+    @Test
+    public void secondNameTest() {}
 }
