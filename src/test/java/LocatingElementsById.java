788bc304d0812aea6f10745d4d1716a048c78cf3
Adam Cegiełka <adamcegielka@tlen.pl>
2024-08-31 15:36:24 +0200

    test: add new test with find element by ID

---
+++ src/test/java/LocatingElementsById.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LocatingElementsById {
+
+    @Test
+    public void findElementById() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+//        driver.findElement(By.id("clickOnMe")).click();
+        By buttonId = By.id("clickOnMe");
+        WebElement clickOnMeButton = driver.findElement(buttonId);
+        clickOnMeButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        Assert.assertEquals("Hello world!", alertText);
+
+        alert.accept();
+
+        driver.quit();
+    }
+}
